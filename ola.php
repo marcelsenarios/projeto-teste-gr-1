78bbc7d73f23bfd3536ea764f19ee7694700e2e3
Marcel <marcelsenarios@gmail.com>
2017-12-07 14:40:28 -0200

    Trabalhando as informações vindas do formulários

--- ola.php
+++ ola.php
@@ -1,5 +1,10 @@
 <?php
 
+// echo '<pre>';
+// 	print_r($_POST);
+// echo '</pre>';
+
+
 $nome 		 = $_POST['nome'];
 $email 		 = $_POST['email'];
 $idade 		 = $_POST['idade'];
@@ -15,8 +20,87 @@ $foto        = $_POST['foto'];
 
 
 
+function retornaNomeSexo($sexo) {
+
+	if($sexo == "M") {
+		$sexoFinal = "Masculino";
+	} else if($sexo == "F") {
+		$sexoFinal = "Feminino";
+	} else if($sexo == "O") {
+		$sexoFinal = "Outros";	
+	} else {
+		$sexoFinal = "Indefinido";
+	}
+
+	return $sexoFinal;
+}
 
 
+function retornaNomeTime($time) {
+	switch ($time) {
+		case 1:
+			$timeFinal = 'Corinthians';
+			break;
+		case 2:
+			$timeFinal = 'Palmeiras';
+			break;
+		case 3:
+			$timeFinal = 'São Paulo';
+			break;
+		case 4:
+			$timeFinal = 'Santos';
+			break;					
+		default:
+			$timeFinal = "Time não informado";
+			break;
+	}
+
+	return $timeFinal;
+}
+
+
+
+function retornaNomeCor($cor) {
+	switch ($cor) {
+		case 1:
+			$corFinal = 'Azul';
+			break;
+		case 2:
+			$corFinal = 'Branco';
+			break;
+		case 3:
+			$corFinal = 'Cinza';
+			break;
+		case 4:
+			$corFinal = 'Vermelho';
+			break;	
+		case 5:
+			$corFinal = 'Verde';
+			break;								
+		default:
+			$corFinal = "Cor não informada";
+			break;
+	}
+
+	return $corFinal;	
+}
+
+
+
+//echo count($cores);
+
+
+foreach ($cores as $cor) {
+	$nomeCor .= retornaNomeCor($cor). " - ";
+}
+
+
+/*
+for($x = 0; $x <= count($cores); $x++ ) {
+
+	$nomeCor .= retornaNomeCor($cores[$x]). " - ";
+}
+*/
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -30,7 +114,6 @@ $foto        = $_POST['foto'];
 			require_once 'include/header.php';
 		?>
 	<!-- FIM TOPO-->
-
 
 	<div class="container-fluid">
 		<div class="col-md-12">
@@ -50,11 +133,11 @@ $foto        = $_POST['foto'];
 				 echo "<hr>";
 				 echo "Idade: ".$idade; 
 				 echo "<hr>";
-				 echo "Sexo: ".$sexo; 
+				 echo "Sexo: ".retornaNomeSexo($sexo); 
 				 echo "<hr>";
-				 echo "Time: ".$time; 
+				 echo "Time: ".retornaNomeTime($time); 
 				 echo "<hr>";		
-				 echo "Cores: ".$cores; 
+				 echo "Cores: ".$nomeCor; 
 				 echo "<hr>";
 				 echo "Observações: ".$observacoes; 
 				 echo "<hr>";				 				 		 				 

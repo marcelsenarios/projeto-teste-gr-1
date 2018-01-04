01d53516de351a92e812be6900b1644421f439ef
Marcel <marcelsenarios@gmail.com>
2018-01-04 16:43:14 -0200

    Criação do menu de fotos dinamico

--- fotos.php
+++ fotos.php
@@ -1,3 +1,6 @@
+<?php
+$foto = $_GET['foto'];
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -24,7 +27,61 @@
 				
 
 
-				<div class="jumbotron"><img src="images/avangers.jpg" class="img-responsive"></div>
+				<div class="jumbotron">
+
+					<?php
+					// $array_fotos = array(
+					// 	"avangers.jpg",
+					// 	"batman.jpg",
+					// 	"dontpanic.jpg",
+					// 	"evilcorp.jpg",
+					// 	"ironman.jpg",
+					// 	"mario.jpg",
+					// 	"mrrobot.png",
+					// 	"oculos.jpg",
+					// 	"pi.jpg",
+					// 	"starwars.jpg",
+					// 	"westworld.png"
+					// );
+
+					// $array_titulo = array(
+					// 	"Avangers",
+					// 	"Batman",
+					// 	"Dont Panic",
+					// 	"Evil Corp",
+					// 	"Iron Man",
+					// 	"Super Mario",
+					// 	"Mr Robot",
+					// 	"Oculos",
+					// 	"PI",
+					// 	"Star Wars",
+					// 	"West World"
+					// );	
+
+
+					$array = array(
+					array("foto" => "avangers.jpg", "titulo" => "Avangers", "info" => "sad sadsadsad sad sa dsad asda"),
+					array("foto" => "batman.jpg", "titulo" => "Batman", "info" => "sad Dsd54s87ds d87s d7s98d 7s98d 7as"),
+					array("foto" => "dontpanic.jpg", "titulo" => "Dont Panic", "info" => ""),
+					array("foto" => "evilcorp.jpg", "titulo" => "Evil Corp", "info" => ""),
+					array("foto" => "ironman.jpg", "titulo" => "Iron Man", "info" => ""),
+					array("foto" => "mario.jpg", "titulo" => "Mario", "info" => ""),
+					array("foto" => "mrrobot.png", "titulo" => "Mr Robot", "info" => ""),
+					array("foto" => "oculos.jpg", "titulo" => "Oculos", "info" => ""),
+					array("foto" => "pi.jpg", "titulo" => "PI", "info" => ""),
+					array("foto" => "starwars.jpg", "titulo" => "Star Wars", "info" => ""),
+					array("foto" => "westworld.png", "titulo" => "West World", "info" => "")
+					);				
+
+
+					//print_r($array);
+					?>
+					
+
+					<h1><?php echo $array[$foto]['titulo']; ?></h1>		
+					<img src="images/<?php echo $array[$foto]['foto']; ?>" class="img-responsive">
+					<p><?php echo $array[$foto]['info']; ?></p>
+				</div>
 
 
 
--- include/menu_lateral.php
+++ include/menu_lateral.php
@@ -1,16 +1,16 @@
 	<div class="col-md-3">
       <div class="list-group">
-        <a href="#" class="list-group-item active">Overview <span class="sr-only">(current)</span></a>
-        <a href="#" class="list-group-item">Avanger</a>
-        <a href="#" class="list-group-item">Batman</a>
-        <a href="#" class="list-group-item">Dont Panic</a>
-        <a href="#" class="list-group-item">Evil Corp</a>
-        <a href="#" class="list-group-item">Iron Man</a>
-        <a href="#" class="list-group-item">Mario</a>
-        <a href="#" class="list-group-item">Mr Robot</a>
-        <a href="#" class="list-group-item">Oculos</a>
-        <a href="#" class="list-group-item">PI</a>
-        <a href="#" class="list-group-item">Star Wars</a>
-        <a href="#" class="list-group-item">West World</a>
+        <a href="#" class="list-group-item active">Fotos</span></a>
+        <a href="fotos.php?foto=0" class="list-group-item">Avanger</a>
+        <a href="fotos.php?foto=1" class="list-group-item">Batman</a>
+        <a href="fotos.php?foto=2" class="list-group-item">Dont Panic</a>
+        <a href="fotos.php?foto=3" class="list-group-item">Evil Corp</a>
+        <a href="fotos.php?foto=4" class="list-group-item">Iron Man</a>
+        <a href="fotos.php?foto=5" class="list-group-item">Mario</a>
+        <a href="fotos.php?foto=6" class="list-group-item">Mr Robot</a>
+        <a href="fotos.php?foto=7" class="list-group-item">Oculos</a>
+        <a href="fotos.php?foto=8" class="list-group-item">PI</a>
+        <a href="fotos.php?foto=9" class="list-group-item">Star Wars</a>
+        <a href="fotos.php?foto=10" class="list-group-item">West World</a>
       </div>
 	</div>
